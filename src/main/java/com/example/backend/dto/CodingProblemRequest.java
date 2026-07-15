69ef2403ca2a7d3538e7564fb5e08e7da5a6657d
예령 <yaeryung93@gmail.com>
2026-07-16 08:14:32 +0900

    서버살리기

--- src/main/java/com/example/backend/dto/CodingProblemRequest.java
+++ src/main/java/com/example/backend/dto/CodingProblemRequest.java
@@ -3,6 +3,8 @@ package com.example.backend.dto;
 public class CodingProblemRequest {
     private Long userId;
     private String code;
-    public Long getUserId() { return userId; } public void setUserId(Long value) { userId = value; }
-    public String getCode() { return code; } public void setCode(String value) { code = value; }
+    public Long getUserId() { return userId; }
+    public void setUserId(Long value) { userId = value; }
+    public String getCode() { return code; }
+    public void setCode(String value) { code = value; }
 }
